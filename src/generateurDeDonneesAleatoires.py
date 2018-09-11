a245bff32771dd1c13145c15bf5017a0ea02dc9e
paul <jacobs.paul.pro@gmail.com>
2018-09-11 14:36:23 -0400

    ajout de la generation des donnees aleatoires

---
+++ src/generateurDeDonneesAleatoires.py
@@ -0,0 +1,33 @@
+import random
+import donneeDAO
+import time
+import json
+from pymongo import MongoClient
+
+
+
+
+
+running=True
+
+while running:
+    data=[]
+    data.append(("\"temperature\"",random.uniform(-10,40))) #generate random temperature
+    data.append(("\"salinity\"",random.uniform(0,100))) #generate random salinity in %
+    data.append(("\"difraction\"",random.uniform(0,100))) #generate random difraction in %
+    
+    formatedString="{"
+    for i in data:
+        formatedString+=str(i[0])
+        formatedString+=":"
+        formatedString+="\""+str(i[1])+"\""
+        formatedString+=","
+    
+    formatedString=formatedString[:-1]
+    formatedString+="}"
+    print(formatedString)
+    donneeDAO.writeValues(json.loads(formatedString))
+
+    time.sleep(2)
+
+
