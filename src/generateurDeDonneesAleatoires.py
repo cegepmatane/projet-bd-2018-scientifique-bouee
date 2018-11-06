e3da44295f6685aea7b694a1e7733faf38e68cc4
Florian <flolevy33@gmail.com>
2018-11-06 15:41:10 -0500

    ajoutDonneesReel

--- src/generateurDeDonneesAleatoires.py
+++ src/generateurDeDonneesAleatoires.py
@@ -9,9 +9,17 @@ running=True
 #boucle generant 10 donnees par seconde
 while running:
     donnee=[]
-    donnee.append(("\"temperature\"",random.uniform(-10,40))) #generation de temperature aleatoire
-    donnee.append(("\"salinity\"",random.uniform(0,100))) #generation de salinite aleatoire en %
-    donnee.append(("\"difraction\"",random.uniform(0,100))) #generation de difraction aleatoire en %
+    donnee.append(("\"temperatureAir\"",random.uniform(-10,40))) #generation de temperature aleatoire
+    donnee.append(("\"temperatureEau\"",random.uniform(-10,40))) #generation de temperature aleatoire
+    donnee.append(("\"directionVent\"",random.uniform(-10,40))) #generation de temperature aleatoire
+    donnee.append(("\"kilometrageVent\"",random.uniform(-10,40))) #generation de temperature aleatoire
+    donnee.append(("\"hauteurMaximum\"",random.uniform(-10,40))) #generation de temperature aleatoire
+    donnee.append(("\"vagueMoyenne\"",random.uniform(-10,40))) #generation de temperature aleatoire
+    donnee.append(("\"periodeVague\"",random.uniform(-10,40))) #generation de temperature aleatoire
+    donnee.append(("\"humidite\"",random.uniform(-10,40))) #generation de temperature aleatoire
+    donnee.append(("\"rafales\"",random.uniform(-10,40))) #generation de temperature aleatoire
+    donnee.append(("\"salaniteEau\"",random.uniform(0,100))) #generation de salinite aleatoire en %
+    donnee.append(("\"densiteeEau\"",random.uniform(0,100))) #generation de difraction aleatoire en %
    
     stringFormatee="{"
     for i in donnee:
